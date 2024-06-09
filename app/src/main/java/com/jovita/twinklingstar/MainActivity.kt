c14945fe41460ee85c99e89b48b868828aec2f74
Jovita John <jovitajohn641@gmail.com>
2024-06-09 20:30:51 +0100

    My base single star

---
+++ app/src/main/java/com/jovita/twinklingstar/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.jovita.twinklingstar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.jovita.twinklingstar.ui.theme.TwinklingStarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TwinklingStarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TwinklingStarBase()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TwinklingStarBase( modifier: Modifier = Modifier) {
+    Box {
+        AnimatedPreloader(modifier = Modifier.size(200.dp).align(Alignment.Center))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    TwinklingStarTheme {
+        TwinklingStarBase()
+    }
+}
+
+@Composable
+fun AnimatedPreloader(modifier: Modifier = Modifier) {
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.star
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+
+    LottieAnimation(
+        composition = preloaderLottieComposition,
+        progress = preloaderProgress,
+        modifier = modifier
+    )
+}
